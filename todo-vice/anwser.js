49ee0f5705579e95270e334079161a0998161060
TracyChen0505 <tracychen0505@gmail.com>
2024-08-14 18:17:16 +0800

    Remove unused CSS and JavaScript files

--- todo-vice/anwser.js
+++ todo-vice/anwser.js
@@ -2,7 +2,8 @@
 const inputElement = document.querySelector(".input");
 const addBtn = document.querySelector(".topbutton");
 const todoList = document.querySelector(".todo-list");
-
+const completedList = document.querySelector(".completed-list");
+const todoinput = document.querySelector(".todolistinput");
 const people = {
   name: "Tom",
 };
@@ -15,6 +16,14 @@ function handleClick() {
   const checkbox = document.createElement("input");
   checkbox.setAttribute("type", "checkbox");
   checkbox.setAttribute("name", "task");
+  checkbox.addEventListener("change", handleChange);
+  function handleChange() {
+    if (checkbox.checked) {
+      completedList.appendChild(newItem);
+    } else {
+      todoList.appendChild(newItem);
+    }
+  }
 
   const label = document.createElement("span");
   label.innerHTML = inputElement.value;
@@ -22,7 +31,6 @@ function handleClick() {
   const deleteButton = document.createElement("button");
   deleteButton.setAttribute("type", "submit");
   deleteButton.innerHTML = "Delete";
-
   deleteButton.addEventListener("click", handleDelete);
 
   newItem.appendChild(checkbox);
@@ -37,4 +45,35 @@ function handleClick() {
     // todoList.removeChild(newItem);
     newItem.remove();
   }
+}
+
+const toDoListUl = document.querySelector(".todo-list");
+
+todoinput.addEventListener("input", handleInput);
+
+function handleInput() {
+  const displayArray = [];
+  const keyword = todoinput.value;
+  for (let item of toDoListUl.children) {
+    console.dir(item);
+    const span = item.querySelector("span");
+    const todoText = span.textContent;
+
+    console.log({
+      todoText,
+      keyword,
+    });
+
+    if (todoText.includes(keyword)) {
+      displayArray.push(item);
+    }
+  }
+
+  toDoListUl.innerHTML = "";
+
+  for (const item of displayArray) {
+    toDoListUl.appendChild(item);
+  }
+
+  console.dir(displayArray);
 }
